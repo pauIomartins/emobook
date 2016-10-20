46be5f863c976d25931a8bc2114bbca85618392b
Paulo Roberto Martins <paulo.martins@email.com>
2016-10-20 20:59:37 -0200

    initial commit

---
+++ assets/js/spa.js
@@ -0,0 +1,17 @@
+angular.module('Platzi', []);
+angular.module('Platzi', []).controller('BaseCrtl', ['$scope', function($scope) {
+
+	io.socket.get('/emoji', function (data) {
+		$scope.emojis = data;
+		$scope.$apply();
+	});
+
+	io.socket.on('emoji', function (event) {
+		switch (event.verb) {
+			case 'created':
+				$scope.emojis.push(event.data);
+				$scope.$apply();
+				break;
+		}
+	});
+}]);
